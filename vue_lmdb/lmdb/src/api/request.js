e4ae80365bc6e88bebafdd4bebac4600a564c954
mxq <yzwmxq@gmail.com>
2022-08-24 22:29:34 +0800

    请求超时时间修改

--- vue_lmdb/lmdb/src/api/request.js
+++ vue_lmdb/lmdb/src/api/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 // 创建一个 axios 实例
 const service = axios.create({
 	baseURL: 'http://localhost:9090/v1', // 所有的请求地址前缀部分
-	timeout: 60000, // 请求超时时间毫秒
+	timeout: 600000, // 请求超时时间毫秒
 	headers: {
 	},
 })
